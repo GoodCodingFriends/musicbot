83b98a98f1c4ac8d419f39e395be40bae5e82442
Yuki Ito <acomagu@gmail.com>
2019-06-13 18:51:00 +0900

    No leave/join from VC in changing music

--- soundplayer/soundplayer.go
+++ soundplayer/soundplayer.go
@@ -28,7 +28,7 @@ func NewSoundPlayer(joiner VoiceChannelJoiner, guildID string) *SoundPlayer {
 		sp: &soundPlayer{
 			GuildID:      guildID,
 			Joiner:       joiner,
-			idletime:     10 * time.Second,
+			idletime:     10 * time.Minute,
 			speakbuftime: 250 * time.Millisecond,
 		},
 		guildID: guildID,
